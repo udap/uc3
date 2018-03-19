4a8702c2f482cdd23ae2ac19926d6c807eb1d1d2
王坤 <wkbjlyzx@163.com>
2018-03-19 14:59:28 +0800

    增加部署的脚本

--- truffle.js
+++ truffle.js
@@ -1,4 +1,16 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*" // Match any network id
+        },
+        test: {
+            host: "127.0.0.1",
+            port: 9545,
+            network_id: "*" // Match any network id
+        }
+    }
 };
