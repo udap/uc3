d16402ac79bc2a9c4e90ae6b2de25682844b99e7
王坤 <wkbjlyzx@163.com>
2018-04-18 17:27:46 +0800

    modify index.html

--- app/js/index.js
+++ app/js/index.js
@@ -1,89 +1,49 @@
 // Import libraries
-import { default as Web3} from 'web3';
+import { default as Web3} from 'web3'
 import { default as contract } from 'truffle-contract'
 
 // Import our contract artifacts and turn them into usable abstractions.
 import assetRegistry_artifacts from '../../build/contracts/AssetRegistry.json'
 
+import {Warrant,Product} from './warrant.js';
+
+import {accounts,account,assetRegistryAddress} from './common'
+
+
 // MetaCoin is our usable abstraction, which we'll use through the code below.
 var AssetRegistry = contract(assetRegistry_artifacts);
+
 
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
 // For application bootstrapping, check out window.addEventListener below.
-var accounts;
-var account;
-var assetRegistryAddress;
 
-window.Issue = {
-    start: function() {
-        var self = this;
 
-        AssetRegistry.setProvider(web3.currentProvider);
+window.Show = {
+    listWarrant: function () {
+        var self = this
+        let warrant = this.getData();
 
-        web3.eth.getAccounts(function(err, accs) {
-            if (err != null) {
-                alert("There was an error fetching your accounts.");
-                return;
-            }
+        let promise = "";
+        if(assetRegistryAddress){
+            promise = AssetRegistry.at(assetRegistryAddress);
+        }else{
+            promise = AssetRegistry.deployed();
+        }
+        let assetRegistry;
 
-            if (accs.length == 0) {
-                alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-                return;
-            }
-            accounts = accs;
-            account = accounts[0];
-        });
-        self.refreshBalance();
-    },
-    refreshBalance: function() {
-        var self = this;
-
-        var meta;
-        AssetRegistry.deployed().then(function(instance) {
-            meta = instance;
-            return meta.getBalance.call(account, {from: account});
-        }).then(function(value) {
-            var balance_element = document.getElementById("balance");
-            balance_element.innerHTML = value.valueOf();
-        }).catch(function(e) {
-            console.log(e);
-            self.setStatus("Error getting balance; see log.");
-        });
-    },
-    issue: function() {
-        var self = this;
-
-        var amount = parseInt(document.getElementById("amount").value);
-        var receiver = document.getElementById("receiver").value;
-
-        this.setStatus("Initiating transaction... (please wait)");
-
-        var meta;
-        MetaCoin.deployed().then(function(instance) {
-            meta = instance;
-            return meta.sendCoin(receiver, amount, {from: account});
-        }).then(function() {
-            self.setStatus("Transaction complete!");
-            self.refreshBalance();
-        }).catch(function(e) {
-            console.log(e);
-            self.setStatus("Error sending coin; see log.");
-        });
+        promise.then(function (instance) {
+            assetRegistryAddress = instance.address;
+            assetRegistry = instance;
+            return assetRegistry.createAsset("",true,false,JSON.stringify(warrant),"",{from:account});
+        }).then(function (result) {
+            window.location.href="index.html";
+        }).catch(function (e) {
+            console.log(e)
+        })
     }
-};
+}
 
-window.addEventListener('load', function() {
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-        console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-        // Use Mist/MetaMask's provider
-        window.web3 = new Web3(web3.currentProvider);
-    } else {
-        console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-        window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
-    }
-
+window.addEventListener('load', function () {
     Issue.start();
-});
+})
