c7d98b7ec09385bf21d66cd0c5585df2732473d8
王坤 <wkbjlyzx@163.com>
2018-04-23 14:22:48 +0800

    add infura network

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "asthma type over scout banana kite cactus flame oppose pizza wrist goose";
+
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // to customize your Truffle configuration!
@@ -10,11 +14,17 @@ module.exports = {
             gas: 3100000
         },
         rinkeby: {
-            host: "127.0.0.1",
-            port: 8545,
-            network_id: "*", // Match any network id
-            from: "0x67f09ED73F2Fe18965d6f35325Ec983Aff2532e6",//private key c2e3c1dec1ca3b77f6e8d58aac4919f930d7653ea44f735810b79800ffadc7f4
+            provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/HUkrzYiEqgEioBDoT5Mq"),
+            network_id: "*"
         },
+        /*kovan: {
+            provider: function () {
+                mnemonics.kovan = process.env.MNEMONICS || mnemonics.kovan || prompt('network kovan mnemonic: ');
+                return new HDWalletProvider(mnemonics.kovan, "https://kovan.infura.io/");rinkeby.etherscan.io
+            },
+            gasPrice: 1000000000,
+            network_id: 42
+        },*/
         test: {
             host: "127.0.0.1",
             port: 9545,
