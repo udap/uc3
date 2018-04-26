dfc15c9659d2fcb56e5803b4f9b52a37c357d9de
王坤 <wkbjlyzx@163.com>
2018-04-26 10:30:04 +0800

    Remove useless code

--- app/src/util/getWeb3.js
+++ app/src/util/getWeb3.js
@@ -6,7 +6,6 @@ let getWeb3 = new Promise(function(resolve, reject) {
     var results;
     var web3 = window.web3;
 
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     if (typeof web3 !== 'undefined') {
       // Use Mist/MetaMask's provider.
       web3 = new Web3(web3.currentProvider)
@@ -19,8 +18,6 @@ let getWeb3 = new Promise(function(resolve, reject) {
 
       resolve(results)
     } else {
-      // Fallback to localhost if no web3 injection. We've configured this to
-      // use the development console's port by default.
       var provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545')
 
       web3 = new Web3(provider);
