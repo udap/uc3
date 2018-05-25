eaf7d560d22aaf45a425f03a987022df15db84aa
王坤 <wkbjlyzx@163.com>
2018-05-25 11:23:03 +0800

    update StandardAsset

--- contracts/StandardAsset.sol
+++ contracts/StandardAsset.sol
@@ -2,25 +2,31 @@ pragma solidity ^0.4.19;
 
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
 
+contract AssetType {
+    string public name;
+    string public symbol;
+    string public uri;
+    constructor(string _name, string _symbol,string _uri){
+        name = _name;
+        symbol = _symbol;
+        uri = _uri;
+    }
+}
+
 contract StandardAsset is ERC721Token {
 
-    enum State {
-        ISSUED, APPROVED, REJECTED
-    }
-
-     uint256 internal tokenId_;
+     uint256 internal id_;
 
     // Mapping from token ID to issuer
     mapping (uint256 => address) internal tokenIssuers;
 
-    // Mapping from token ID to State
-    mapping (uint256 => State) internal tokenStates;
+    AssetType private assetType;
 
     /**
    * @dev Constructor function
    */
-    constructor(string _name, string _symbol) public ERC721Token(_name,_symbol) {
-
+    constructor(string _name, string _symbol,string _classURI) public ERC721Token(_name,_symbol) {
+        assetType = new AssetType(_name,_symbol,_classURI);
     }
 
     /**
@@ -29,13 +35,12 @@ contract StandardAsset is ERC721Token {
        * @param _to address the beneficiary that will own the minted token
        * @param _uri token uri
        */
-    function issue(address _to,string _uri) public {
-        uint256 tokenId = tokenId_ ++ ;
+    function mint(address _to,string _tokenURI) public {
+        uint256 tokenId = id_ ++ ;
         require(!exists(tokenId));
         super._mint(_to, tokenId);
-        super._setTokenURI(tokenId,_uri);
+        super._setTokenURI(tokenId,_tokenURI);
         tokenIssuers[tokenId] = msg.sender;
-        tokenStates[tokenId] = State.ISSUED;
     }
 
     /**
@@ -44,23 +49,9 @@ contract StandardAsset is ERC721Token {
    * @param _owner owner of the token to burn
    * @param _tokenId uint256 ID of the token being burned by the msg.sender
    */
-    function destory(address _owner, uint256 _tokenId) public {
-//        require(tokenState[_tokenId] != State.APPROVED);
+    function burn(address _owner, uint256 _tokenId) public {
         super._burn(_owner, _tokenId);
         delete tokenIssuers[_tokenId];
-        delete tokenStates[_tokenId];
-    }
-
-    /**
-  * @dev set the token State for a given token
-  * @dev Reverts if the token ID does not exist
-  * @param _tokenId uint256 ID of the token to set its State
-  * @param _state new  State
-  */
-    function setTokenState(uint256 _tokenId, State _state) public onlyOwnerOf(_tokenId) {
-        require(tokenStates[_tokenId] == State.ISSUED);
-        require(exists(_tokenId));
-        tokenStates[_tokenId] = _state;
     }
 
     /**
@@ -73,15 +64,6 @@ contract StandardAsset is ERC721Token {
         return tokenIssuers[_tokenId];
     }
 
-    /**
-       * @dev Returns state for a given token ID
-       * @dev Throws if the token ID does not exist.
-       * @param _tokenId uint256 ID of the token to query
-       */
-    function tokenState(uint256 _tokenId) public view returns (State) {
-        require(exists(_tokenId));
-        return tokenStates[_tokenId];
-    }
     /**
    * @dev Returns token IDs of owner
    * @param _owner token owner
