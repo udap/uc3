ec9925549000f7cfe4dfcdb09a2ea6d9d2e0a107
王坤 <wkbjlyzx@163.com>
2018-05-25 14:44:07 +0800

    add annotations from AssetRegistry

--- contracts/AssetRegistry.sol
+++ contracts/AssetRegistry.sol
@@ -17,14 +17,31 @@ contract AssetRegistry is Ownable {
 
     }
 
+    /**
+    * @dev  Registering a type of asset
+    * @param _name AssetType name
+    * @param _symbol AssetType symbol
+    */
     function registerAssetClass(string _name,string _symbol) onlyOwner public returns(uint){
         return register(_name,_symbol,"");
     }
 
+    /**
+    * @dev  Registering a type of asset
+    * @param _name AssetType name
+    * @param _symbol AssetType symbol
+    * @param _uri AssetType metadata uri
+    */
     function registerAssetClass(string _name,string _symbol,string _uri) onlyOwner public returns(uint){
         return register(_name,_symbol,_uri);
     }
 
+    /**
+    * @dev internal function, Registering a type of asset
+    * @param _name AssetType name
+    * @param _symbol AssetType symbol
+    * @param _uri AssetType metadata uri
+    */
     function register(string _name,string _symbol,string _uri) internal returns(uint){
         uint id = getId(_name, _symbol,_uri);
         if(idAssets[id] != address(0x0)){
@@ -36,10 +53,17 @@ contract AssetRegistry is Ownable {
         return id;
     }
 
-    function getAssetClass(uint id) public view returns(StandardAsset){
-        return idAssets[id];
+    /**
+    * @dev Query asset by id
+    * @param _id 'AssetType''s id
+    */
+    function getAssetClass(uint _id) public view returns(StandardAsset){
+        return idAssets[_id];
     }
 
+    /**
+    * @dev id generator
+    */
     function getId(string _name,string _symbol,string _uri) internal pure returns(uint){
         uint id = uint(keccak256(abi.encodePacked(_name, _symbol,_uri)));
         return id;
