8ecdc0f43d303fc1145fa1694b0af82f84df71bc
王坤 <wkbjlyzx@163.com>
2018-08-10 13:40:44 +0800

    fix bug

--- server/common/result.js
+++ server/common/result.js
@@ -1,8 +1,8 @@
 
 const success = content => {
     let ret = {
-        code: 1,
-        message: "success"
+        retcode: 1,
+        msg: "success"
     };
     if (content)
         ret.content = content;
@@ -11,8 +11,8 @@ const success = content => {
 
 const fail = (message,content) => {
     let ret = {
-        code: 0,
-        message: message?message:"fail"
+        retcode: 0,
+        msg: message?message:"fail"
     };
     if (content)
         ret.content = content;
@@ -21,10 +21,10 @@ const fail = (message,content) => {
 
 const other = (code,message,content) => {
     let ret = {
-        code:code?code:0
+        retcode:code?code:0
     };
     if (message)
-        ret.message = message;
+        ret.msg = message;
     if (content)
         ret.content = content;
     return ret;
