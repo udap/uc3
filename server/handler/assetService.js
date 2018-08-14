08d65c4a1964fb75b5c4bf5b93849f4508bec329
王坤 <wkbjlyzx@163.com>
2018-08-14 19:34:14 +0800

    request localhost

--- server/handler/assetService.js
+++ server/handler/assetService.js
@@ -129,7 +129,7 @@ const getAllByOwner =async (ctx) => {
     tokenURIs.forEach((item,index)=>{
         let pro;
         if (item && (item.startsWith("http") || item.startsWith("https"))){
-            pro = request.get(item.replace("ipfs.io","ipfs.infura.io")).then(res =>{
+            pro = request.get(item.replace("ipfs.io","127.0.0.1")).then(res =>{
                 return res.body;
             });
         }else {
