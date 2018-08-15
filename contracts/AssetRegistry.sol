c340a61ca2e46c11a7bccb2b7eff52ce8a1a1598
Li Zhang <cybertriton@gmail.com>
2018-08-15 16:42:09 +0800

    Update AssetRegistry.sol

--- contracts/AssetRegistry.sol
+++ contracts/AssetRegistry.sol
@@ -5,7 +5,7 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract AssetRegistry is Ownable {
 
-    event StandardAssetCreated(StandardAsset indexed asset, uint indexed id);
+    event AssetRegistered(StandardAsset indexed asset, uint indexed id);
 
     // Mapping from contract ID to StandardAsset
     mapping(uint => StandardAsset) public idAssets;
@@ -15,23 +15,32 @@ contract AssetRegistry is Ownable {
     }
 
     /**
-    * @dev  Registering a type of asset
+    * @dev  Registering a type of asset that has at most one asset
     * @param _name AssetType name
     * @param _symbol AssetType symbol
     */
     function registerClass(string _name, string _symbol) onlyOwner public returns (uint){
-        return register(_name, _symbol, "");
+        return register(_name, _symbol, 1, "");
     }
 
     /**
     * @dev  Registering a type of asset
     * @param _name AssetType name
     * @param _symbol AssetType symbol
+    * @param _supplyLimit supply limit
     * @param _uri AssetType metadata uri
     */
-    function registerClass(string _name, string _symbol, string _uri) onlyOwner public returns (uint){
-        return register(_name, _symbol, _uri);
+    function registerClass(string _name, string _symbol, uint256 _supplyLimit, string _uri) onlyOwner public returns (uint){
+        return register(_name, _symbol, _supplyLimit, _uri);
     }
+    
+    function registerClass(AssetType _type) onlyOwner public returns (uint) {
+        uint id = getId(_type.name(), _type.symbol(), _type.uri());
+        require(idAssets[id] == address(0x0));
+        idAssets[id] = _type;
+        emit AssetRegistered(_type, id);    
+        return id;
+    }    
 
     /**
     * @dev internal function, Registering a type of asset
@@ -39,21 +48,21 @@ contract AssetRegistry is Ownable {
     * @param _symbol AssetType symbol
     * @param _uri AssetType metadata uri
     */
-    function register(string _name, string _symbol, string _uri) internal returns (uint){
+    function register(string _name, string _symbol, uint256 _supplyLimit, string _uri) internal returns (uint){
         uint id = getId(_name, _symbol, _uri);
         require(idAssets[id] == address(0x0));
-        StandardAsset asset = new StandardAsset(_name, _symbol, _uri);
+        StandardAsset asset = new StandardAsset(_name, _symbol, _supplyLimit, _uri);
         idAssets[id] = asset;
-        emit StandardAssetCreated(asset, id);
+        emit AssetRegistered(asset, id);
+        return id;
     }
 
     /**
     * @dev id generator
     */
     function getId(string _name, string _symbol, string _uri) public pure returns (uint){
-        uint id = uint(keccak256(abi.encodePacked(_name, _symbol, _uri)));
+        uint256 id = uint256(keccak256(abi.encodePacked(_name, _symbol, _uri)));
         return id;
     }
-
 
 }
