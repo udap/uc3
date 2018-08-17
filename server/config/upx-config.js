05254ec212a520837e7c418216e33cc55b4ad219
王坤 <wkbjlyzx@163.com>
2018-08-17 17:25:21 +0800

    update logo

--- server/config/upx-config.js
+++ server/config/upx-config.js
@@ -2,7 +2,7 @@ var config = {
     address:"0x68f2CBdA42eDb8f2A4Da853b34E772A5f8e98Ce0",
     name:"UDAP Token",
     symbol:"UPX",
-    icon:"https://www.udap.io/img/udap-logo.png"
+    icon:"https://www.udap.io/logo400x400.png"
 };
 
 module.exports = config;
