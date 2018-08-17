0eab59f2dacdd39c456ca7b89e73ee5209eecb87
王坤 <wkbjlyzx@163.com>
2018-08-17 19:35:40 +0800

    add some data

--- server/testData.js
+++ server/testData.js
@@ -1,37 +1,42 @@
 
-
-
-
-
 const data =  {
     "1":{
         name:"The Godfather",
         desc:"“The Godfather” is told entirely within a closed world. That’s why we sympathize with characters who are essentially evil. The story by Mario Puzoand Francis Ford Coppola is a brilliant conjuring act, inviting us to consider the Mafia entirely on its own terms. Don Vito Corleone (Marlon Brando) emerges as a sympathetic and even admirable character; during the entire film, this lifelong professional criminal does nothing of which we can really disapprove.",
-        image:"http://47.104.225.39:3000/img/1.jpg",
+        image:"http://47.104.225.39:3000/img/godfather.jpg",
         attributes:{
-            cinema:"Wanda cinema",
-            site:"3 rows , 3 rows",
-            address:"Room 201, 18 Tianchang Road, Huangyan District，Taizhou City，Zhejiang Province"
+            CINEMA:"Wanda cinema",
+            SEAT:"D12",
+            SCREEN:"6",
+            ADDRESS:"Room 201, 18 Tianchang Road, Huangyan District，Taizhou City，Zhejiang Province",
+            DATE:"25/08/2018",
+            TIME:"14:00PM"
         }
     },
     "2":{
         name:"The Godfather",
         desc:"“The Godfather” is told entirely within a closed world. That’s why we sympathize with characters who are essentially evil. The story by Mario Puzoand Francis Ford Coppola is a brilliant conjuring act, inviting us to consider the Mafia entirely on its own terms. Don Vito Corleone (Marlon Brando) emerges as a sympathetic and even admirable character; during the entire film, this lifelong professional criminal does nothing of which we can really disapprove.",
-        image:"http://47.104.225.39:3000/img/2.jpg",
+        image:"http://47.104.225.39:3000/img/godfather.jpg",
         attributes:{
-            cinema:"Wanda cinema",
-            site:"31 rows , 2 rows",
-            address:"Room 201, 18 Tianchang Road, Huangyan District，Taizhou City，Zhejiang Province"
+            CINEMA:"Wanda cinema",
+            SEAT:"D13",
+            SCREEN:"6",
+            ADDRESS:"Room 201, 18 Tianchang Road, Huangyan District，Taizhou City，Zhejiang Province",
+            DATE:"25/08/2018",
+            TIME:"14:00PM"
         }
     },
     "3":{
         name:"The Godfather",
         desc:"“The Godfather” is told entirely within a closed world. That’s why we sympathize with characters who are essentially evil. The story by Mario Puzoand Francis Ford Coppola is a brilliant conjuring act, inviting us to consider the Mafia entirely on its own terms. Don Vito Corleone (Marlon Brando) emerges as a sympathetic and even admirable character; during the entire film, this lifelong professional criminal does nothing of which we can really disapprove.",
-        image:"http://47.104.225.39:3000/img/3.jpg",
+        image:"http://47.104.225.39:3000/img/godfather.jpg",
         attributes:{
-            cinema:"Wanda cinema",
-            site:"13 rows , 15 rows",
-            address:"Room 201, 18 Tianchang Road, Huangyan District，Taizhou City，Zhejiang Province"
+            CINEMA:"Wanda cinema",
+            SEAT:"D14",
+            SCREEN:"6",
+            ADDRESS:"Room 201, 18 Tianchang Road, Huangyan District，Taizhou City，Zhejiang Province",
+            DATE:"25/08/2018",
+            TIME:"14:00PM"
         }
     }
 
