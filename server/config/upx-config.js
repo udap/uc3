161e7fc752e116cdd8ca1c44e2a05d3501bb55db
王坤 <wkbjlyzx@163.com>
2018-08-17 15:39:23 +0800

    add icon

--- server/config/upx-config.js
+++ server/config/upx-config.js
@@ -2,6 +2,7 @@ var config = {
     address:"0x68f2CBdA42eDb8f2A4Da853b34E772A5f8e98Ce0",
     name:"UDAP Token",
     symbol:"UPX",
+    icon:"https://www.udap.io/img/udap-logo.png"
 };
 
 module.exports = config;
