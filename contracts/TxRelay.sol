3ff9be0effefab03418bac5ff4c7a7203d2d2f9e
王坤 <wkbjlyzx@163.com>
2018-09-01 19:41:38 +0800

    add Relay contract

--- contracts/TxRelay.sol
+++ contracts/TxRelay.sol
@@ -1,7 +1,53 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.19;
 
-contract TxRelay {
-    function TxRelay(){
 
+import 'openzeppelin-solidity/contracts/ECRecovery.sol';
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
+
+contract TxRelay is Ownable {
+
+    using ECRecovery for bytes32;
+
+    using SafeMath for uint256;
+
+    mapping(address => uint) nonce;
+
+    mapping(address => uint256) balance;
+
+
+    /*
+    * @dev Relays meta transactions
+    */
+    function relayMetaTx(address _txSender,address _dest,bytes _data,uint256 _fees,bytes _sig) public {
+        require(balance[_txSender] >= _fees);
+        bytes32 hash = keccak256(abi.encodePacked(this,_txSender,_dest,_data,nonce[_txSender],_fees));
+        address addr = hash.recover(_sig);
+        require(addr == _txSender);
+        nonce[_txSender]++; //if we are going to do tx, update nonce
+        balance = balance.sub(_fees);
+        require(_dest.call(data));
+    }
+
+
+    /*
+     * @dev Returns the local nonce of an account.
+     * @param add The address to return the nonce for.
+     * @return The specific-to-this-contract nonce of the address provided
+     */
+    function getNonce(address addr) public constant returns (uint) {
+        return nonce[addr];
+    }
+
+    function withdrawal() public{
+        msg.sender.transfer(balance[msg.sender]);
+    }
+
+    function ownerWithdrawal() public onlyOwner{
+        msg.sender.transfer(this.balance);
+    }
+
+    function() payable public {
+        balance = balance.add(msg.value);
     }
 }
