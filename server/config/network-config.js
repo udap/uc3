57f8cf98d6091e38083d12276a899ba19d8520e3
王坤 <wkbjlyzx@163.com>
2018-09-06 20:41:51 +0800

    change network

--- server/config/network-config.js
+++ server/config/network-config.js
@@ -4,8 +4,8 @@ let config ={
         nodeEndpoint:"http://52.11.73.193:6969/child_0"
     },
     ropsten:{
-        serviceEndpoint:"https://wallet.chainmind.io",
-        nodeEndpoint:"http://206.189.164.83:8545"
+        serviceEndpoint:"http://47.104.225.39:3001",
+        nodeEndpoint:"http://47.104.225.39:8545"
     }
 };
 
