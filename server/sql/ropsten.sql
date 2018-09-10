d74c8a9604cb61fb0e631022abdc4106dc69fc43
王坤 <wkbjlyzx@163.com>
2018-09-10 15:24:26 +0800

    add sql

--- server/sql/ropsten.sql
+++ server/sql/ropsten.sql
@@ -2,5 +2,5 @@ INSERT INTO `asset_type` (`id`, `gid`, `address`, `metadata`, `name`, `symbol`, 
 VALUES ('1', '0', '0x68f2CBdA42eDb8f2A4Da853b34E772A5f8e98Ce0', '{\"icon\":\"https://www.udap.io/logo400x400.png\"}', 'UDAP Token', 'UPX', NULL, '1', 'ERC20', '2018-09-07 14:45:47', '2018-09-07 15:38:58');
 
 
-INSERT INTO `ropsten`.`view_template` (`id`, `type_id`, `key`, `context`, `template_uri`, `create_time`, `update_time`)
-VALUES ('1', '8', 'view', 'https://wallet.chainmind.io/template/certificate', '/view.html', '2018-09-08 20:35:31', '2018-09-08 20:37:25');
+INSERT INTO `view_template` (`id`, `type_id`, `key`, `context`, `template_uri`, `thumbnail`, `create_time`, `update_time`)
+VALUES ('1107', '12', 'view', 'https://wallet.chainmind.io/template/certificate', '/view.html', '/view-thumbnail.png', '2018-09-08 12:35:31', '2018-09-10 13:39:05');
