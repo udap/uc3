45274a4da17f3aea48686d2bc977988de2c84f45
王坤 <wkbjlyzx@163.com>
2018-09-22 10:34:27 +0800

    add test code

--- server/test/testEthereum.js
+++ server/test/testEthereum.js
@@ -3,10 +3,14 @@ const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io
 
 const standardAsset_artifacts = require('../../build/contracts/StandardAsset.json');
 let abi = standardAsset_artifacts.abi;
+
+let startGetData = new Date().getTime();
 let standardAsset = web3.eth.contract(abi).at("0x68f2CBdA42eDb8f2A4Da853b34E772A5f8e98Ce0");
 let data = standardAsset.balanceOf.getData("0x110189c51e9b9eebee1ae111c8f28718cf795fef");
 
-console.log("data=====",data);
+let endGetData = new Date().getTime();
+
+console.log("data spend time =====",endGetData - startGetData);
 
 
 
@@ -14,8 +18,14 @@ const contract = require('truffle-contract');
 const StandardAsset = contract(standardAsset_artifacts);
 StandardAsset.setProvider(web3.currentProvider);
 
+let startInstance = new Date().getTime();
 //StandardAsset.at("xxxxxx") Spent a lot of time
+let endInstance = startInstance;
 StandardAsset.at("0x68f2CBdA42eDb8f2A4Da853b34E772A5f8e98Ce0").then(instance=>{
-    console.log("获取完成了instance=======================");
-    console.log(instance);
+    endInstance = new Date().getTime();
+    console.log("instance spend time =======",endInstance - startInstance);
+    let owner = "0x110189c51e9b9eebee1ae111c8f28718cf795fef";
+    return instance.balanceOf.call(owner,{from: owner});
+}).then(balance=>{
+    console.log("get balance spend time ===",new Date().getTime() - endInstance);
 })
