e675a711607837e96889cb51779f1fccbf6f596d
王坤 <wkbjlyzx@163.com>
2018-09-29 18:21:53 +0800

    network-config.js  description

--- server/config/network-config.js
+++ server/config/network-config.js
@@ -2,12 +2,14 @@ let config ={
     pchain:{
         serviceEndpoint:"http://47.104.225.39",
         nodeEndpoint:"http://18.144.39.0:6969/child_8",
-        currency:"PAI"
+        currency:"PAI",
+        description:"Pchain Test Network"
     },
     ropsten:{
         serviceEndpoint:"http://47.104.225.39",
         nodeEndpoint:"http://47.104.225.39:8545",
-        currency:"ETH"
+        currency:"ETH",
+        description:"Ethereum Test Network"
     }
 };
 
