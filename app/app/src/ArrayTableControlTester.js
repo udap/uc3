9a9e6ed13611deef0af6fbf84afbc7697196a0b0
张丹 <zhangdan@xinongtech.com>
2018-10-01 15:57:34 +0800

    update

--- app/app/src/ArrayTableControlTester.js
+++
@@ -1,7 +0,0 @@
-/* import { isObjectArrayControl, optionIs } from '@jsonforms/core'
-
-export default isObjectArrayControl(optionIs('layout', 'VertcalLayout'))
-*/
-import { isObjectArrayControl, scopeEndsWith } from '@jsonforms/core'
-
-export default isObjectArrayControl(scopeEndsWith('comments'))
