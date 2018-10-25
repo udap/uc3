1841f312c46b1d5b70b69d739fdda2ba910ded48
王坤 <wkbjlyzx@163.com>
2018-10-25 11:49:08 +0800

    fix bug

--- contracts/ens/Resolver.sol
+++ contracts/ens/Resolver.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.24;
 
 
 import "./ENS.sol";
@@ -21,7 +21,7 @@ contract Resolver {
         ens = ENS(ensAddr);
     }
 
-    function addr(bytes32 node)  constant public returns (address) {
+    function addr(bytes32 node)  view public returns (address) {
         return addresses[node];
     }
 
