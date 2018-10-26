40a49cadb8dbc606316d1129235951098e4dc617
王坤 <wkbjlyzx@163.com>
2018-10-26 14:32:32 +0800

    register subdomain

--- server/handler/ensService.js
+++ server/handler/ensService.js
@@ -7,8 +7,13 @@ const Result = require('../common/result');
 const udapValidator = require('../common/udapValidator');
 const ethereumUtil = require('../util/ethereumUtil');
 const signUtil = require('../util/signUtil');
-
-
+const web3Util = require('web3-utils');
+const contract = require('truffle-contract');
+const harvestRegistrar_artifacts = require('../../build/contracts/HarvestRegistrar.json');
+const HarvestRegistrar = contract(harvestRegistrar_artifacts);
+HarvestRegistrar.setProvider(web3.currentProvider);
+const harvestRegistrarAddr = harvestRegistrar_artifacts.networks[web3.version.network];
+const namehash = require('eth-ens-namehash');
 
 const getAddrByDomain = async (ctx) => {
 
@@ -19,7 +24,7 @@ const getAddrByDomain = async (ctx) => {
     let domain = fields.domain;
     let appid = fields.appid;
     if (!udapValidator.isDomainName(domain))
-        ctx.throw("'domainName' param error");
+        ctx.throw("'domain' param error");
     await udapValidator.appidRegistered(appid);
     let address =await ens.resolver(domain).addr().then( addr => {
         return addr;
@@ -50,6 +55,25 @@ const registerSubDomain = async (ctx) => {
     let caller = ctx.header["x-identity"];
     let domain = fields.domain;
     let sig = fields.sig;
+
+    // valid param
+    if (!web3.isAddress(caller))
+        ctx.throw("'caller' param error");
+    if (!udapValidator.isDomainName(domain))
+        ctx.throw("'domain' param error");
+    if (!sig)
+        ctx.throw("'sig' param error");
+
+    let nonce = await HarvestRegistrar.deployed().then(instance=>{
+        return instance.nonces(caller);
+    });
+    let sha = web3Util.soliditySha3(
+        {type: 'address', value: harvestRegistrarAddr},
+        {type: 'uint256', value: nonce.toNumber()},
+        {type: 'string', value: domain},
+        {type: 'address', value: caller});
+    if(!signUtil.verifyEcsign(Buffer.from(sha.substr(2), 'hex'),sig,caller))
+        ctx.throw("Signature error");
 
     // valid data
     let address = fields.address;
