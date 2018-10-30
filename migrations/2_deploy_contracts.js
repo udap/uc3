474f5985e72d1c878a1b720cabecf0352bc073d5
王坤 <wkbjlyzx@163.com>
2018-10-30 14:52:45 +0800

    fix bug

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,11 @@
 var HarvestRegistrar = artifacts.require("./ens/HarvestRegistrar.sol");
 
 let ens = "0x112234455c3a32fd11230c42e7bccd4a84e02010";
-let name = "udaptest";
 let defaultResolver = "0x4c641fb9bad9b60ef180c31f56051ce826d21a9a" ;
 
 module.exports = function(deployer,network) {
     if (network == "ropsten") {
-        deployer.deploy(HarvestRegistrar,ens, name,defaultResolver);
+        deployer.deploy(HarvestRegistrar,ens,defaultResolver);
     } else {
     }
 };
