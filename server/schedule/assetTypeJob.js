9e27f0b4bbdb9106c017031bee248eeb7173bbb7
王坤 <wkbjlyzx@163.com>
2018-11-08 12:16:34 +0800

    decode transactionReceipt logs

--- server/schedule/assetTypeJob.js
+++ server/schedule/assetTypeJob.js
@@ -6,8 +6,17 @@ const TxSent = require('../model/txSent');
 const txStatus = require('../common/txStatus');
 const txBizType = require('../common/txBizType');
 const DomainModel = require('../model/domain');
+let SolidityEvent = require("web3/lib/web3/event.js");
+const assetRegistry_artifacts = require('../../build/contracts/AssetRegistry.json');
+const assetRegistryEvents = assetRegistry_artifacts.abi.filter( json => {
+    return json.type === 'event';
+}).map(json => {
+    // note first and third params required only by enocde and execute;
+    // so don't call those!
+    return new SolidityEvent(null, json, null);
+});
 
-const queryCreateTypeReceipt = ()=>{
+/*const queryCreateTypeReceipt = ()=>{
     setInterval(()=>{
         console.log("start queryCreateTypeReceipt job..");
         AssetType.findAll(
@@ -28,6 +37,46 @@ const queryCreateTypeReceipt = ()=>{
                         updateFields,
                         { where: { id: item.id }}
                     );
+                }
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+        console.log("end queryCreateTypeReceipt job..");
+    },1 * 60 * 1000);
+};*/
+const queryCreateTypeReceipt = ()=>{
+    setInterval(()=>{
+        console.log("start queryCreateTypeReceipt job..");
+        AssetType.findAll(
+            { where: {status:2} }
+        ).then(typeList =>{
+            typeList.forEach((item, index)=>{
+                let receipt = web3.eth.getTransactionReceipt(item.txHash);
+                console.log("receipt=======,",receipt);
+                let updateFields = {};
+                if(receipt && receipt.status){
+                    if(receipt.logs){
+                        let events = receipt.logs.map( log => {
+                            return assetRegistryEvents.find(decoder => {
+                                return (decoder.signature() == log.topics[0].replace("0x",""));
+                            }).decode(log);
+                        });
+                        let assetRegisterEvent = events.find(element => {
+                            return element.event == 'AssetRegistered';
+                        });
+                        if(assetRegisterEvent){
+                            let args = assetRegisterEvent.args;
+                            updateFields.address = args.asset;
+                        }
+                    }
+                    updateFields.status = parseInt(receipt.status,16);
+                    if (Object.keys(updateFields.length > 0)){
+                        AssetType.update(
+                            updateFields,
+                            { where: { id: item.id }}
+                        );
+                    }
                 }
             });
         }).catch((err) => {
